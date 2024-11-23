5e2271d193850ce3f7567cead82a57517e529bf6
sohseyedi <sohseyedi@gmail.com>
2024-11-23 19:00:16 +0330

    homeAccordion component and order submission button to main section layout.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,13 @@
 <template>
-  <section class="max-w-4xl lg:px-0 px-3 container text-[#fefefe] mx-auto py-5">
-    <AccordionContainer
-     v-for="acc in accordions"
-      :key="acc.id" 
-      :title="acc.title"
-      :isOpen="activeAccordion === acc.id"
-      @toggle="handleToggle(acc.id)"
-      :visible="acc.id"
-    />
-    <Footer/>
+  <section class="max-w-4xl lg:px-0 px-4 container text-[#fefefe] mx-auto py-5">
+    <HomeAccordion/>
+    <footer class="mt-3 flex items-center">
+      <button class="text-[#141414] bg-green-500 font-semibold w-[180px] h-[45px] text-lg rounded-xl flex items-center justify-center">
+          ثبت سفارش
+      </button>
+    </footer>
   </section>
 </template>
 <script setup lang="ts">
-import AccordionContainer from '../components/AccordionContainer.vue';
-import Footer from '../components/Footer.vue'
-import { ref } from 'vue';
-
-const accordions = [
-  { id: 1, title: 'اطلاعات کاربر' },
-  { id: 2, title: 'اطلاعات سفارش' },
-  { id: 3, title: 'جزئیات سفارش' },
-];
-
-const activeAccordion = ref<number | null>(0);
-
-const handleToggle = (id: number) => {
-  activeAccordion.value = activeAccordion.value === id ? null : id;
-};
+import HomeAccordion from '@/components/accordion/HomeAccordion.vue';
 </script>
