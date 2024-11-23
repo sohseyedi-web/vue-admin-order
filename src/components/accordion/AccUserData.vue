5b4ec43c4eef6f6282799740ed7fda1cd8e94706
sohseyedi <sohseyedi@gmail.com>
2024-11-23 19:05:40 +0330

    form inputs for personal details including name, phone number, address, and postal code with responsive layout

---
+++ src/components/accordion/AccUserData.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="lg:py-3 pb-4 flex items-center gap-3 flex-wrap lg:flex-row flex-col">
+        <input 
+            placeholder="نام و نام خانوادگی" 
+            type="text" 
+            class="lg:w-[49%] w-full my-2 h-[45px] text-[#141414] text-lg font-semibold rounded-xl bg-white px-3 outline-none"
+            >
+        <input 
+            placeholder="شماره موبایل" 
+            type="text" 
+            class="lg:w-[49%] w-full my-2 h-[45px] text-[#141414] text-lg font-semibold rounded-xl bg-white px-3 outline-none"
+            >
+        <input 
+            placeholder="آدرس" 
+            type="text" 
+            class="lg:w-[70%] w-full my-2 h-[45px] text-[#141414] text-lg font-semibold rounded-xl bg-white px-3 outline-none"
+            >
+        <input 
+            placeholder="کد پستی" 
+            type="text" 
+            class="lg:w-[28%] w-full my-2 h-[45px] text-[#141414] text-lg font-semibold rounded-xl bg-white px-3 outline-none"
+            >
+    </section>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
