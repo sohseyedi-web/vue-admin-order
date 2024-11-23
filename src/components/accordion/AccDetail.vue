ef03a228a69756d0902ca40ad7c28149b7a63942
sohseyedi <sohseyedi@gmail.com>
2024-11-23 19:04:59 +0330

    RowDetails component with nested checkboxes for order options and implement Persian number formatting with commas

---
+++ src/components/accordion/AccDetail.vue
@@ -0,0 +1,16 @@
+<template>
+        <RowDetails :title="toPersianNumbersWithComma('50000')" price="تومان">
+            <Checkbox label="مشتری مایل است سفارش به صورت تکشویی انجام شود"/>
+        </RowDetails>
+        <RowDetails :title="toPersianNumbersWithComma('30000')" price="تومان">
+            <Checkbox label="سفارش به صورت اکسپرس انجام شود"/>
+        </RowDetails>
+        <RowDetails title="زمان تحویل سفارش ها حداکثر 3 روز بعد از ثبت میباشد.">
+            <Checkbox label="دریافت سفارش در زمان دیگر"/>
+        </RowDetails>
+</template>
+<script setup lang="ts">
+import RowDetails from '../../ui/RowDetails.vue';
+import { toPersianNumbersWithComma } from '../../utils/toPersianNumber';
+import Checkbox from '../Checkbox.vue';
+</script>
