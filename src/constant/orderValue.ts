f6e53349c90c2c357b7ee3e0b437f7a024c55c8b
sohseyedi <sohseyedi@gmail.com>
2024-11-23 18:58:13 +0330

    dummy order list items

---
+++ src/constant/orderValue.ts
@@ -0,0 +1,64 @@
+export const orderLists = [
+    {
+      id: 1,
+      value: 0,
+      label: "کت",
+      price: 30000,
+    },
+    {
+      id: 2,
+      value: 0,
+      label: "شلوار",
+      price: 10000,
+    },
+  
+    {
+      id: 3,
+      value: 0,
+      label: "تی شرت",
+      price: 10000,
+    },
+    {
+      id: 4,
+      value: 0,
+      label: "دشداشه",
+      price: 20000,
+    },
+    {
+      id: 5,
+      value: 0,
+      label: "پتو تک نفره",
+      price: 50000,
+    },
+    {
+      id: 6,
+      value: 0,
+      label: "پتو دو نفره",
+      price: 70000,
+    },
+    {
+      id: 7,
+      value: 0,
+      label: "پرده",
+      price: 50000,
+    },
+    {
+      id: 8,
+      value: 0,
+      label: "کفش",
+      price: 25000,
+    },
+    {
+      id: 9,
+      value: 0,
+      label: "کیف",
+      price: 15000,
+    },
+    {
+      id: 10,
+      value: 0,
+      label: "مانتو",
+      price: 10000,
+    },
+  ];
+  
