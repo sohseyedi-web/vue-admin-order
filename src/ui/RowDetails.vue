1cb65bfb522c13442b905f408ed1689d286c0c33
sohseyedi <sohseyedi@gmail.com>
2024-11-23 19:00:35 +0330

    reusable component to display a title with optional price in a flex layout.

---
+++ src/ui/RowDetails.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex lg:items-center gap-2 lg:flex-row flex-col">
+        <slot></slot>
+        <p class="text-zinc-700 text-sm">({{ title }} {{ price }})</p>
+    </div>
+</template>
+<script setup lang="ts">
+    const {title,price} = defineProps({
+        title:{
+            type : String,
+            required:true
+        },
+        price:{
+            type : String,
+        }
+    })
+</script>
