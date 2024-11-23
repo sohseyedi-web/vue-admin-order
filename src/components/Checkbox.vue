b43eaa05a7f3104c32fa3a8a08b06ec3798db49e
sohseyedi <sohseyedi@gmail.com>
2024-11-23 19:00:52 +0330

    checkbox component

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="form-control my-3">
+        <label class="cursor-pointer flex gap-x-2 items-center">
+          <input
+            type="checkbox"
+            class="checkbox checkbox-success border-2"
+            v-model="isChecked"
+          />
+          <span class="label-text font-semibold">
+            {{ label }}        
+        </span>
+        </label>
+      </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref } from 'vue';
+
+  const {label} = defineProps({
+    label:{
+        type:String
+    }
+  })
+  
+  const isChecked = ref(false);
+  </script>
+  
