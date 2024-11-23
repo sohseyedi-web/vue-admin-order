0a65972e917e697f9133b778b4ff06b5c82e0d6d
sohseyedi <sohseyedi@gmail.com>
2024-11-23 19:06:53 +0330

    add image order for save prev submit order

---
+++ src/components/order/ImageOrder.vue
@@ -0,0 +1,11 @@
+<template>
+    <section class="flex items-center gap-x-3 my-2 flex-wrap">
+        <div class="w-[150px] cursor-pointer flex-col rounded-xl bg-white flex items-center justify-center h-[150px]">
+            <PlusIcon class="size-10 text-zinc-800"/>
+            <span class="text-zinc-800 font-semibold text-lg">اضافه کردن <br>عکس سفارش</span>
+        </div>
+    </section>
+</template>
+<script setup lang="ts">
+import { PlusIcon } from '@heroicons/vue/20/solid';
+</script>
