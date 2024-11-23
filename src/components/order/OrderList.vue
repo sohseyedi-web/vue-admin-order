bfdbc4dc7eb81135d5a3a40f65c926233a8b429b
sohseyedi <sohseyedi@gmail.com>
2024-11-23 19:03:42 +0330

    create a scrollable order list section with dynamic item rendering.

---
+++ src/components/order/OrderList.vue
@@ -0,0 +1,17 @@
+<template>
+    <section
+      class="w-full h-[235px] overflow-y-auto cursor-pointer text-lg rounded-xl"
+    >
+      <div class="flex items-center flex-wrap lg:gap-3 gap-y-5 justify-between p-3 shadow-sm">
+        <div v-for="item in orderLists" :key="item.id" class="flex items-center justify-between gap-x-2 md:w-[45%] w-full">
+          <OrderItem :value="item.value" :label="item.label"/>
+        </div>
+      </div>
+    </section>
+  </template>
+<script setup lang="ts">
+import { orderLists } from '@/constant/orderValue';
+import OrderItem from './OrderItem.vue';
+
+
+</script>
